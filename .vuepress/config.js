ffde9d056077f003a040c28d4ab5a602fe9f22e5
awallach4 <32426187+awallach4@users.noreply.github.com>
2022-06-29 17:38:15 -0600

    Added basic vuepress config.

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Hotplate Sites",
+  description: "An Open Source Site Builder",
+  themeConfig: {
+    repo: "awallach4/hotplate-sites",
+    sidebar: [
+      "/",
+      "/installation"
+    ]
+  }
+}
