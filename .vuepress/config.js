9f59db4f43d19b18cb8a1e338791cb9af0683b76
awallach4 <32426187+awallach4@users.noreply.github.com>
2022-06-29 17:56:38 -0600

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
   title: "Hotplate Sites",
   description: "An Open Source Site Builder",
+  head: [
+    ["meta", {name: "viewport", content: "width=device-width,initial-scale=1.0"}],
+    ["meta", {charset: "utf-8"}],
+    ["link", {rel: "icon", href: "/favicon-32x32.png", sizes: "32x32"}]
+  ]
   themeConfig: {
     repo: "awallach4/hotplate-sites",
     sidebar: [
